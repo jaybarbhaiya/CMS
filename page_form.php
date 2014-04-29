ce8dffb506d6651034590ab71b61039ad27a5c3d
Jay Bhargav Barbhaiya <jaybarb1990@gmail.com>
2014-04-29 16:57:08 +0200

    hiding non-visible subjects and pages to indiex.php,page_form.php

--- page_form.php
+++ page_form.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php // this page is included by new_page.php and edit_page.php ?>
 <?php if (!isset($new_page)) {$new_page = false;} ?>
 
@@ -32,38 +31,3 @@
 <p>Content:<br />
 	<textarea name="content" rows="20" cols="80" required="required"><?php echo $sel_page['content']; ?></textarea>
 </p>
-=======
-<?php // this page is included by new_page.php and edit_page.php ?>
-<?php if (!isset($new_page)) {$new_page = false;} ?>
-
-<p>Page name: <input type="text" name="menu_name" required="required" value="<?php echo $sel_page['menu_name']; ?>" id="menu_name" /></p>
-
-<p>Position: <select name="position">
-	<?php
-		if (!$new_page) {
-			$page_set = get_pages_for_subject($sel_page['subject_id']);
-			$page_count = mysql_num_rows($page_set);
-		} else {
-			$page_set = get_pages_for_subject($sel_subject['id']);
-			$page_count = mysql_num_rows($page_set) + 1;
-		}
-		for ($count=1; $count <= $page_count; $count++) {
-			echo "<option value=\"{$count}\"";
-			if ($sel_page['position'] == $count) { echo " selected"; }
-			echo ">{$count}</option>";
-		}
-	?>
-</select></p>
-<p>Visible: 
-	<input type="radio" name="visible" required="required" value="0"<?php 
-	if ($sel_page['visible'] == 0) { echo " checked"; } 
-	?> /> No
-	&nbsp;
-	<input type="radio" name="visible" required="required" value="1"<?php 
-	if ($sel_page['visible'] == 1) { echo " checked"; } 
-	?> /> Yes
-</p>
-<p>Content:<br />
-	<textarea name="content" rows="20" cols="80" required="required"><?php echo $sel_page['content']; ?></textarea>
-</p>
->>>>>>> 56e5d9cd1d5ecdf17073b138341982d073c9a813
